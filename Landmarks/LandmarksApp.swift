e0a78cdbd262430d526778862eb7d03ad2e7eeb6
Johnathan Bevers <beversjohnathan7@gmail.com>
2022-07-14 21:45:34 -0500

    Base App Template Created

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Johnathan Bevers on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
