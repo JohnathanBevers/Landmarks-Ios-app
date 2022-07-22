ed2c8fe3eb915860e81941cc110f0eba647cb5d6
Johnathan Bevers <beversjohnathan7@gmail.com>
2022-07-21 22:09:59 -0500

    Set Up Navigation Between List and Detail

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,38 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height:220)
-                .ignoresSafeArea(edges: .top)
-            CircleImage()
-                .offset(y:-130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                
-                Text("Description Text")
-            }
-            .padding()
-            
-            Spacer()
+        LandmarkList()
         }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
